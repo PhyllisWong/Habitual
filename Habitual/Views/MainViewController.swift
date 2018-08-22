6184cb9b638f07d1fe55700b289645d1ac592c3f
PhyllisWong <wong.phyllism@gmail.com>
2018-08-22 14:44:08 -0700

    Add TableView delegate methods

--- Habitual/Views/MainViewController.swift
+++ Habitual/Views/MainViewController.swift
@@ -12,7 +12,14 @@ class MainViewController: UIViewController {
   
   override func viewDidLoad() {
     setupNavBar()
+    // FIXME: Uncomment the next 2 lines when the tableview outlet is added
+//    tableView.delegate = self
+//    tableView.dataSource = self
   }
+    
+    let nib = UINib(nibName: "CustomCell", bundle: nil)
+    // FIXME: Uncomment this line when the tableview outlet is added
+//    tableView.register(nib, forCellReuseIdentifier: "habitCell")
   
 }
 
@@ -34,4 +41,26 @@ extension MainViewController {
   @objc func addHabit(sender: UIBarButtonItem) {
     print("Add button tapped")
   }
+}
+
+// MARK: - Tableview Delegate Methods
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    // Sets the number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Sets the number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // Configure the cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "habitCell") as! HabitTableViewCell
+        
+        return cell
+    }
+    
 }
